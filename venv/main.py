d782e0fcf97d8600cdc9d245efb270479a338a74
aekik1118 <aekin1118@gmail.com>
2019-08-01 21:05:42 +0900

    10초 단위로 time window 에 따라 로그 저장

--- venv/main.py
+++ venv/main.py
@@ -1,5 +1,5 @@
 from parsezeeklogs import ParseZeekLogs
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 
 def conv_raw_record_to_datetime(raw):
@@ -9,20 +9,50 @@ def conv_raw_record_to_datetime(raw):
     ret = datetime.fromtimestamp((float)(raw))
     return ret
 
+flag_is_first = True
+log_start_flag = True
+log_record_flag = False
+ex_datetime = datetime.fromtimestamp(100000.1)
+feature_record_list = []
+
 with open('out.csv',"w") as outfile:
     for log_record in ParseZeekLogs("conn.log", output_format="csv", safe_headers=False, fields=["ts","id.orig_h","id.orig_p","id.resp_h","id.resp_p"]):
         if log_record is not None:
             log_record_list = log_record.split(',')
-            feature_record_list = []
 
             #log의 timestamp 를 datetime으로 변환
             log_record_datetime = conv_raw_record_to_datetime(log_record_list[0])
 
+            if flag_is_first:
+                ex_datetime = log_record_datetime
+                flag_is_first = False
+                log_start_flag = False
+                continue
+
             #time window의 시작 로그인지 체크
-
-
+            if (ex_datetime + timedelta(seconds=10)) < log_record_datetime:
+                log_start_flag = True
+                log_record_flag = True
 
             #추출한 feature를 저장
-            feature_record_list.append(log_record_datetime.strftime('%F %H:%M:%S:%f'))
-            feature_record = ','.join(feature_record_list)
-            outfile.write(feature_record + "\n")
+            if log_record_flag:
+                log_record_flag = False
+                feature_record_list.append(ex_datetime.strftime('%F %H:%M:%S:%f'))
+                feature_record = ','.join(feature_record_list)
+                outfile.write(feature_record + "\n")
+                feature_record_list.clear()
+
+            #시작 로그면 ex_datetime update
+            if log_start_flag:
+                log_start_flag = False
+                ex_datetime = log_record_datetime
+
+
+
+    #마지막 feature 저장
+    feature_record_list.append(ex_datetime.strftime('%F %H:%M:%S:%f'))
+    feature_record = ','.join(feature_record_list)
+    outfile.write(feature_record + "\n")
+
+
+
