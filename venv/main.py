420276b336689910f9f1ac2f22079f9c987df258
aekik1118 <aekin1118@gmail.com>
2019-08-01 20:32:31 +0900

    log에서 timestamp를 추출하여 datetime으로 저장

--- venv/main.py
+++ venv/main.py
@@ -1,1 +1,28 @@
-print("hello")
+from parsezeeklogs import ParseZeekLogs
+from datetime import datetime
+import time
+
+def conv_raw_record_to_datetime(raw):
+    raw = raw[1:-1]
+    if(len(raw) < 1):
+        return datetime.now()
+    ret = datetime.fromtimestamp((float)(raw))
+    return ret
+
+with open('out.csv',"w") as outfile:
+    for log_record in ParseZeekLogs("conn.log", output_format="csv", safe_headers=False, fields=["ts","id.orig_h","id.orig_p","id.resp_h","id.resp_p"]):
+        if log_record is not None:
+            log_record_list = log_record.split(',')
+            feature_record_list = []
+
+            #log의 timestamp 를 datetime으로 변환
+            log_record_datetime = conv_raw_record_to_datetime(log_record_list[0])
+
+            #time window의 시작 로그인지 체크
+
+
+
+            #추출한 feature를 저장
+            feature_record_list.append(log_record_datetime.strftime('%F %H:%M:%S:%f'))
+            feature_record = ','.join(feature_record_list)
+            outfile.write(feature_record + "\n")
